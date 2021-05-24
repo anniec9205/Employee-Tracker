5b3f1da6f11c7a1162ae8dcba27b51728329f907
anniec9205 <danbi9205@gmail.com>
2021-05-24 21:15:36 +1000

    added the npms and the gitignore and js file

--- employee_tracker.js
+++ employee_tracker.js
@@ -14,3 +14,24 @@ var connection = mysql.createConnection({
     password: "123456789",
     database: "employee_tracker_db"
 });
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    askQuestions();
+});
+
+function askQuestions() {
+    inquirer.prompt({
+        message: "what would you like to do?",
+        type: "list",
+        choices: [
+            "view all employees",
+            "view all departments",
+            "add employee",
+            "add department",
+            "add role",
+            "update employee role",
+            "QUIT"
+        ],
+  
